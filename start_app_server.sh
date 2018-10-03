3da7c67259053010a0edda6d4025307e8a92b67b
Michael Leow <mleow@jirnexu.com>
2018-10-03 16:47:56 +0800

    [SECURITY]: Remove sensitive credentials; already rotated in mLabs

--- start_app_server.sh
+++ start_app_server.sh
@@ -4,7 +4,7 @@
 # READ SECRET!
 # export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
 # export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds121183.mlab.com:21183/cooljoe
-export MONGODB_CREDENTIALS=mongodb://leowmjw:passw0rd@ds121183.mlab.com:21183/cooljoe
+# export MONGODB_CREDENTIALS=mongodb://leowmjw:passw0rd@ds121183.mlab.com:21183/cooljoe
 
 # finally, run the app in dev mode...
 npm run dev
