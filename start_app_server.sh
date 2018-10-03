a92ca4747eb82e311636bfb833374ecb4cb0a5db
Michael Leow <mleow@jirnexu.com>
2018-10-03 16:26:26 +0800

    Add hardcoded mlab credentials! Dont DO this!!

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,9 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+# export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+# export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds121183.mlab.com:21183/cooljoe
+export MONGODB_CREDENTIALS=mongodb://leowmjw:passw0rd@ds121183.mlab.com:21183/cooljoe
 
 # finally, run the app in dev mode...
 npm run dev
